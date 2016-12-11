2226c89fdf31c4b663150ddb70462f1ca0d9ff70
dmclough <davidclough@gmail.com>
2016-12-11 14:33:16 -0800

    added random mole switch

--- js/script.js
+++ js/script.js
@@ -5,23 +5,24 @@ $( document ).ready(function() {
 
     setInterval(printTime, 1000);
 
-
 });
 
-var time = 3; //initializes the start time
+var time = 30; //initializes the start time
 
 function printTime() {
   time -= 1;
   $( "#timeRemaining" ).text( "Time remaining:" + time );
   console.log(time);
   showMole();
+  // console.log("show mole in position" + showMole());
 }
-
 
 
 function showMole() {
   var mole1Position = Math.round(Math.random()*16);
-  console.log("mole position 1: " + mole1Position);
+  console.log("mole 1 position: " + mole1Position);
+  document.getElementById(mole1Position).className = "moleShow";
+
   return mole1Position;
 }
 
