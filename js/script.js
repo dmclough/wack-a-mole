e4590eec285ea00a098a50449a19f6b900fc3b87
dmclough <davidclough@gmail.com>
2016-12-11 13:18:22 -0800

    initila commit to push files

--- js/script.js
+++ js/script.js
@@ -1,6 +1,33 @@
 
+$( document ).ready(function() {
+    console.log( "document is ready!" );
 
-//create a 
+    // Attempted to hide the instructions on button click
+    // $("#playGame").click(function() {
+    //   $("#introInfo").slideUp();
+    // });
+
+    setInterval(printTime, 1000);
+
+});
+var time = 30; //initializes the start time
+
+function printTime() {
+  time -= 1;
+  $( "#timeRemaining" ).text( "Time remaining:" + time );
+  console.log(time);
+}
+
+
+
+
+//create a timer that runs at a set interval
 
 
 //create a function to tell if cursor clicked on a div
+
+
+//determine if mole should be shown/hidden
+
+
+//
