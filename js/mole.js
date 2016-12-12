1e2a1f9312221ac408168663bed80b198bf56984
dmclough <davidclough@gmail.com>
2016-12-11 20:38:02 -0800

    changed set interval again

---
+++ js/mole.js
@@ -0,0 +1,30 @@
+$( document ).ready(function() {
+    console.log("document is ready");
+
+    var initialTime = 30;
+    time = initialTime;
+    var countdown;
+
+    countDown = setInterval(tick, 1000);
+
+    function tick() {
+      time--;
+      console.log(time);
+      document.getElementById('timeRemaining').textContent = time;
+      if (time <= 0) {
+        clearInterval(countDown);
+      } else {
+        showMole();
+      }
+    }
+
+
+});
+
+//this function shows the mole if 
+function showMole() {
+  var mole1Position = Math.round(Math.random()*15);
+  console.log("mole 1 position: " + mole1Position);
+  document.getElementById(mole1Position).className = "moleShow";
+  return mole1Position;
+}
