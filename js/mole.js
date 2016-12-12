35bce8f3aefd6c1a8abe1a9f5cd9e8eef764cfd7
dmclough <davidclough@gmail.com>
2016-12-12 09:46:21 -0800

    got hide functionality working

--- js/mole.js
+++ js/mole.js
@@ -1,22 +1,27 @@
 $( document ).ready(function() {
     console.log("document is ready");
 
-    var initialTime = 5;
+    var initialTime = 30;
     time = initialTime;
+    var showInterval = 1000;
     var countdown;
     player1Score = 0;
     player2Score = 0;
     currentPlayer = 1; //player one starts
 
-    countDown = setInterval(tick, 1000);
+    countDown = setInterval(tick, showInterval);
 
     function tick() {
       time--;
       console.log(time);
       document.getElementById('timeRemaining').textContent = "Time remaining: " +time;
+      gameOverCheck();
+    }
+
+    function gameOverCheck() {
       if (time <= 0) {
         clearInterval(countDown);
-        currentPlayer =2;
+        currentPlayer = 2;
         console.log(currentPlayer);
       } else {
         showMole();
@@ -31,12 +36,23 @@ $( document ).ready(function() {
       var mole1Position = Math.round(Math.random()*15);
       console.log("mole 1 position: " + mole1Position);
       document.getElementById(mole1Position).innerHTML = "<button onclick='score()'>Mole!</button>";
+      // setTimeout(hideMoles(mole1Position), hideDelay);
+
+      // hideMoles(mole1Position);
+      hideMoles(mole1Position);
       return mole1Position;
     }
 
-    function hideMoles() {
-      document.getElementById(mole1Position).className = "moleHide";
+    function hideMoles(mole1Position) {
+      var hideDelay = 1100;
+      setTimeout(function() {
+        document.getElementById(mole1Position).innerHTML = "";
+      }, hideDelay);
     }
+
+
+
+
 
     function score() {
       if (currentPlayer === 1) {
@@ -44,7 +60,7 @@ $( document ).ready(function() {
         document.getElementById('play1Score').textContent = "Player 1: "+player1Score;
       } else {
         player2Score++;
-        document.getElementById('play2Score').textContent = "Player 1: "+player1Score;
+        document.getElementById('play2Score').textContent = "Player 2: "+player2Score;
       }
       console.log("player 1 score:" + player1Score);
     }
