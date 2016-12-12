64ad9a18f58d6d25d2c3bc93823b3a9d73fa4951
dmclough <davidclough@gmail.com>
2016-12-11 20:03:16 -0800

    refactored set interval function

--- js/script.js
+++ js/script.js
@@ -1,39 +1,41 @@
-
-
 $( document ).ready(function() {
-    console.log( "document is ready!" );
+    console.log( "document is ready" );
 
-    setInterval(printTime, 1000);
+    var initialTime = 30;
+    time = initialTime;
+
+    var countDown = setInterval(function(){
+        console.log("time running");
+        var timeRemaining = document.getElementById('timer');
+        time--;
+        console.log(time);
+        document.getElementById('timeRemaining').textContent = time;
+          if (time <= 0) {
+            clearInterval(countDown);
+          } else {
+            showMole();
+          }
+      },1000
+    );
+
+
+
+
 
 });
 
-var time = 30; //initializes the start time
 
-function printTime() {
-  time -= 1;
-  $( "#timeRemaining" ).text( "Time remaining:" + time );
-  console.log(time);
-  showMole();
-  // console.log("show mole in position" + showMole());
-}
 
 
 function showMole() {
-  var mole1Position = Math.round(Math.random()*16);
+  // hideMole();
+  var mole1Position = Math.round(Math.random()*15);
   console.log("mole 1 position: " + mole1Position);
   document.getElementById(mole1Position).className = "moleShow";
-
   return mole1Position;
 }
 
-
-//create a timer that runs at a set interval
-
-
-//create a function to tell if cursor clicked on a div
-
-
-//determine if mole should be shown/hidden
-
-
-//
+hideMole();
+function hideMole() {
+  $( "#molePatch" ).children().class( "background-color", "blue" );
+}
