5b23f361f7c14dd39d44206450add2b3548a371e
dmclough <davidclough@gmail.com>
2016-12-12 13:36:55 -0800

    commiting working state with player 1 count.

--- js/mole.js
+++ js/mole.js
@@ -1,7 +1,7 @@
 $( document ).ready(function() {
     console.log("document is ready");
 
-    var initialTime = 5;
+    var initialTime = 30;
     time = initialTime;
     var showInterval = 1000;
     var countdown;
@@ -29,28 +29,25 @@ $( document ).ready(function() {
     function endGame() {
       clearInterval(countDown);
       for (i = 0; i <16; i++) {
-        document.getElementById(i).innerHTML = "";
+        $("#mole"+mole1Position).removeClass("moleShow");
+        $("#mole"+mole1Position).addClass("moleGrass");
       }
       if (currentPlayer == 1) {
         currentPlayer = 2;
+
         //alert("player 2 turn"); //confirm, allows yes or no . Jquery dialog http://jqueryui.com/dialog/#modal-confirmation
 
         tick();// restart the interval and then give it function tick
       }
       // currentPlayer = 2;
-      console.log(currentPlayer);
       return currentPlayer;
     }
-
-    // $( "#molePatch" ).children().click(function() {
-    //     $( "#"+this.id ).hasClass( "moleShow" );
-    //     console.log("howdy");
-    //     alert( "Handler for .click() called." );
-    // });
 
     $( "#molePatch" ).children().click(function() {
       if ( $( this ).hasClass( "moleShow" ) ) {
         console.log("you wacked a mole");
+        $(this).removeClass("moleShow");
+        $(this).addClass("moleGrass");
         incrementScore();
       }
     });
@@ -65,10 +62,6 @@ $( document ).ready(function() {
       }
     }
 
-//Start document ready brackets
-});
-//End document ready brackets
-
 
     //this function shows the mole
     function showMole() {
@@ -81,31 +74,13 @@ $( document ).ready(function() {
     }
 
     function hideMoles(mole1Position) {
-      var hideDelay = Math.random()*2000 + 500;
+      var hideDelay = Math.random()*1000 + 500;
       setTimeout(function() {
         $("#mole"+mole1Position).removeClass("moleShow");
         $("#mole"+mole1Position).addClass("moleGrass");
       }, hideDelay);
     }
 
-
-
-
-
-    // function score(mole1Position) {
-    //   $('#'+mole1Position).click(function() {
-    //       $(this).removeClass('moleShow');
-    //       $(this).addClass('moleGrass');
-    //   });
-    //
-    //
-    //
-    //   if (currentPlayer === 1) {
-    //     player1Score++;
-    //     document.getElementById('play1Score').textContent = "Player 1: "+player1Score;
-    //   } else {
-    //     player2Score++;
-    //     document.getElementById('play2Score').textContent = "Player 2: "+player2Score;
-    //   }
-    //   console.log("player 1 score:" + player1Score);
-    // }
+//Start document ready brackets
+});
+//End document ready brackets
